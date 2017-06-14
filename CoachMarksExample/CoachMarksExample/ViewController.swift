bd697202d981c4045159f40fbed4a8580dbcccf9
Darin Doria <doria@knights.ucf.edu>
2017-06-13 22:50:41 -0400

    Initial commit

---
+++ CoachMarksExample/CoachMarksExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoachMarksExample
+//
+//  Created by Darin Doria on 6/13/17.
+//  Copyright © 2017 Darin Doria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Source/CoachMarks/CoachMarks.h
@@ -0,0 +1,19 @@
+//
+//  CoachMarks.h
+//  CoachMarks
+//
+//  Created by Darin Doria on 6/13/17.
+//  Copyright © 2017 Darin Doria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoachMarks.
+FOUNDATION_EXPORT double CoachMarksVersionNumber;
+
+//! Project version string for CoachMarks.
+FOUNDATION_EXPORT const unsigned char CoachMarksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoachMarks/PublicHeader.h>
+
+
