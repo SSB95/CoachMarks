4f5e5a2f87cb4944ae153e53b4f5179cddb9ca57
Darin Doria <doria@knights.ucf.edu>
2017-06-15 23:30:58 -0400

    Implement view controller in swift 💥

--- CoachMarksExample/CoachMarksExample/ViewController.swift
+++ CoachMarksExample/CoachMarksExample/ViewController.swift
@@ -19,7 +19,35 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let coachMarks = [
+            [
+                "rect": CGRect(x: 6, y: 24, width: 40, height: 40),
+                "caption": "Synchronize your mail",
+                "shape": "circle",
+                "font": UIFont.boldSystemFont(ofSize: 14.0)
+            ],
+            [
+                "rect": CGRect(x: 275, y: 24, width: 40, height: 40),
+                "caption": "Create a new message",
+                "shape": "circle"
+            ],
+            [
+                "rect": CGRect(x: 0, y: 125, width: 320, height: 60),
+                "caption": "Swipe for more options",
+                "shape": "square",
+                "swipe": true
+            ]
+        ]
+        
+        // let coachMarksView = CoachMarksView(with: self.view.bounds, coachMarks: coachMarks)
+        let coachMarksView = CoachMarksView()
+        
+        self.view.addSubview(coachMarksView)
+        coachMarksView.start()
+    }
 }
 
